250795b991b7669deb5d58c718d2899031c6dcd0
Daniel R. Franzini <daniel.franzini@gmail.com>
2022-09-17 22:41:57 -0300

    Fixed authors names and other char issues.

--- modules/mod_wm/mod_wm.c
+++ modules/mod_wm/mod_wm.c
@@ -1,7 +1,7 @@
 /*
- *  Copyright © 2006-2019 SplinterGU (Fenix/Bennugd)
- *  Copyright © 2002-2006 Fenix Team (Fenix)
- *  Copyright © 1999-2002 José Luis Cebrián Pagüe (Fenix)
+ *  Copyright (C) 2006-2019 SplinterGU (Fenix/Bennugd)
+ *  Copyright (C) 2002-2006 Fenix Team (Fenix)
+ *  Copyright (C) 1999-2002 Jose Luis Cebrian Pague (Fenix)
  *
  *  This file is part of Bennu - Game Development
  *
